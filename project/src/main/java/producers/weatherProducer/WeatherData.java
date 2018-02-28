1fb8787b2d7a7245e76ee5be5e236a49d3cd5e1a
André Berberich <Andre.Berberich@tecalliance.net>
2018-02-28 16:21:09 +0100

    Pushed for Adi

---
+++ project/src/main/java/producers/weatherProducer/WeatherData.java
@@ -0,0 +1,9 @@
+package producers.weatherProducer;
+
+public class WeatherData {
+    private static String PLZ;
+
+    public static String getPLZ() {
+        return PLZ;
+    }
+}
---
+++ project/src/main/java/producers/weatherProducer/WeatherProducer.java
@@ -0,0 +1,23 @@
+package producers.weatherProducer;
+
+import producers.BaseProducer;
+import producers.Configuration;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class WeatherProducer extends BaseProducer{
+
+    public WeatherProducer() throws IOException, TimeoutException {
+        this.prepareConnectionAndChannel();
+    }
+
+    public void sendMessage(WeatherData payload) throws IOException {
+        String weatherDataJson = "";
+        this.sendChannel.basicPublish(Configuration.instance.exchangeName, WeatherData.getPLZ(), null, weatherDataJson.getBytes());
+    }
+
+    public void close() throws IOException, TimeoutException {
+        this.closeConnection();
+    }
+}
