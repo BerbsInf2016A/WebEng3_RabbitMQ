02f63b6260d4b1dc0f4b32751e7232e1d0da6ad8
André Berberich <Andre.Berberich@tecalliance.net>
2018-03-01 13:57:28 +0100

    Adjusted base dto to get the type identifier in the generated json

--- project/src/main/java/producers/BaseDTO.java
+++ project/src/main/java/producers/BaseDTO.java
@@ -8,6 +8,11 @@ public abstract class BaseDTO {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss z")
     private final Date creationDate;
     private final String plz;
+
+    public String getTypeIdentifier() {
+        return typeIdentifier;
+    }
+
     private final String typeIdentifier;
 
     public String getPlz() {
